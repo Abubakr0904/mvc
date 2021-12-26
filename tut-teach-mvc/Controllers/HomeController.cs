e5704d09ef26c84a86aa87a8418361d0860a6b41
abubakr0904 <b1903874@mdis.uz>
2021-12-27 00:15:57 +0500

    done

---
+++ tut-teach-mvc/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using tut_teach_mvc.Models;
+
+namespace tut_teach_mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public string Index()
+    {
+        return "This is my <b>default</b> action...";
+    }
+
+    public string Welcome()
+    {
+        return "This is my <b>default</b> action...";
+    }
+}
